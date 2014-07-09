d6084ad4162aad1417206dd606cbf3a43b073ff7
Bartosz Pachołek <bartosz@ideaconnect.pl>
2014-07-09 21:21:21 +0200

    Initial version commit

---
+++ SimpleWebServer/WebServer.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+//added by IDCT
+using System.Collections.Generic;
+using System.IO;
+using System.IO.IsolatedStorage;
+using Windows.Networking.Sockets;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+
+namespace IDCT
+{
+    //method to be called when url rule is met
+    public delegate webResposne RuleDeletage(webResposne response);
+
+    //delegate type for the error event
+    public delegate void ErrorOccured(int code, string message);
+
+    public struct webResposne
+    {
+        public Dictionary<string, string> header;
+        public string method;
+        public string uri;
+        public Stream content;
+    }
+
+    public class WebServer
+    {
+        /// <summary>
+        /// isolated storage instance as server needs to save temp files
+        /// </summary>
+        IsolatedStorageFile isf = IsolatedStorageFile.GetUserStoreForApplication();
+
+        /// <summary>
+        /// indicates if server should be listening
+        /// </summary>
+        protected bool isListening = false;
+
+        /// <summary>
+        /// random number generator
+        /// </summary>
+        protected Random rnd = new Random();
+
+        /// <summary>
+        /// event fired when an error occured
+        /// </summary>
+        public event ErrorOccured errorOccured;
+
+        /// <summary>
+        /// socket listener - the main IO part
+        /// </summary>
+        StreamSocketListener listener = new StreamSocketListener();
+
+        /// <summary>
+        /// rules: Url Regex => method to be called when rule is met
+        /// </summary>
+        private Dictionary<Regex, RuleDeletage> serverRules;
+
+        /// <summary>
+        /// initializes the new server object and puts it in listening mode
+        /// </summary>
+        /// <param name="rules">set of rules in a form "Url Regex" => "Method to be fired when rule is met"</param>
+        /// <param name="ip">ip to bind to</param>
+        /// <param name="port">port to bind to</param>
+        public WebServer(Dictionary<Regex, RuleDeletage> rules, string ip, string port)
+        {
+            //assign passed rules to the server
+            serverRules = rules;
+            try
+            {
+                //try to turn on the server
+                Task.Run(async () =>
+                {
+                    //start listening
+                    listener.ConnectionReceived += listener_ConnectionReceived;
+
+                    //bing do ip:port
+                    await listener.BindEndpointAsync(new Windows.Networking.HostName(ip), port);
+                    
+                    isListening = true;
+                });
+            }
+            catch (Exception ex)
+            {
+                //if possible fire the error event with the exception message
+                if (errorOccured != null)
+                {
+                    errorOccured(-1, ex.Message);
+                }
+            }
+
+        }
+
+        void listener_ConnectionReceived(StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args)
+        {
+            //if we should not be listening anymore, yet for some reason request was still parsed (phone not yet closed the socket) exit the method as it may be unwanted by the user for anybody to read any data
+            if (isListening == false)
+            {
+                return;
+            }
+
+            //write request to a temporary file (better than memory in case of future handling of big post requests
+            string filename = "/temp_" + rnd.Next(100, 999).ToString() + rnd.Next(1000, 9999).ToString();
+
+            //get the request socket
+            StreamSocket sck = args.Socket;
+
+            //create a new file based on the temp file name
+            IsolatedStorageFileStream plik = isf.CreateFile(filename);
+
+            //create a new stream reader object
+            DataReader read = new DataReader(sck.InputStream);
+            read.InputStreamOptions = InputStreamOptions.Partial;
+
+            //defines if we should stop reading
+            bool finished = false;
+
+            //small buffer will make requests long, too big may cut connections (probably something missing in the webserver)
+            uint maxbuf = 4096;
+
+            //create new task
+            Task.Run(async () =>
+            {
+                //yeah yeah I know it is bad, yet does it's job :)
+                while (true)
+                {
+                    //wait for full buffer
+                    await read.LoadAsync(maxbuf);
+
+                    //if there is any data in the buffer
+                    if (read.UnconsumedBufferLength > 0)
+                    {
+                        //create a new byte buffer (for our internal use) long as the data in the socket buffer
+                        uint len = read.UnconsumedBufferLength;
+                        byte[] buffer = new byte[read.UnconsumedBufferLength];
+
+                        //read the data from the socket to the internal buffer
+                        read.ReadBytes(buffer);
+
+                        //write the new data to the temporary file
+                        plik.Write(buffer, 0, buffer.Length);
+
+                        //if buffer was not filled it means that we got the last packet
+                        if (len < maxbuf)
+                        {
+                            finished = true;
+                        }
+                    }
+                    else
+                    {
+                        //no data in the buffer: finished
+                        finished = true;
+                    }
+                    
+                    //if we finished...
+                    if (finished == true)
+                    {
+                        //close the temporary file
+                        plik.Close();
+
+                        //pass it for further parsing
+                        parseRequest(filename, sck);
+
+                        //break the loop
+                        break;
+                    }
+                } //Loop
+            });//Task
+        }
+
+        /// <summary>
+        /// method which parses the request and decides about the action
+        /// </summary>
+        /// <param name="request_file">path to the temporary file holding the request packet</param>
+        /// <param name="socket">socket used with the request (required for response)</param>
+        public void parseRequest(string request_file, StreamSocket socket)
+        {
+            //open and read the request: needs stream reading support for large files
+            IsolatedStorageFileStream plika = isf.OpenFile(request_file, FileMode.Open);
+            StreamReader reada = new StreamReader(plika);
+
+            //read the first ling to get request type
+            string linge = reada.ReadLine();
+
+            //create new request object (same type as response object - possibly better class naming needs to be used)
+            webResposne request = new webResposne();
+
+            //if there is any data...
+            if (linge != null && linge.Length > 0)
+            {
+                //get the method (currently GET only supported really) and the request URL
+                if (linge.Substring(0, 3) == "GET")
+                {
+                    request.method = "GET";
+                    request.uri = linge.Substring(4);
+                }
+                else if (linge.Substring(0, 4) == "POST")
+                {
+                    request.method = "POST";
+                    request.uri = linge.Substring(5);
+                }
+                
+                //remove the HTTP version
+                request.uri = Regex.Replace(request.uri, " HTTP.*$", "");
+
+                //create a dictionary for the sent headers
+                Dictionary<string, string> headers = new Dictionary<string, string>();
+
+                //read HTTP headers into the dictionary
+                string line = "";
+                do
+                {
+                    line = reada.ReadLine();
+                    string[] sepa = new string[1];
+                    sepa[0] = ":";
+                    string[] elems = line.Split(sepa, 2, StringSplitOptions.RemoveEmptyEntries);
+                    if (elems.Length > 0)
+                    {
+                        headers.Add(elems[0], elems[1]);
+                    }
+                } while (line.Length > 0);
+
+                //assign headers to the request object
+                request.header = headers;
+
+                //assigne rest of the content to the request object in a form of a stream handle moved to the part with content after previous read line operations
+                request.content = reada.BaseStream;
+
+                //determines if we found a matching URL rule
+                bool foundrule = false;
+                
+                //create a stream writer to the output stream (response)
+                DataWriter writ = new DataWriter(socket.OutputStream);
+
+                //if there are any server rules
+                if (serverRules != null)
+                {
+                    //for every rule...
+                    foreach (Regex rule_part in serverRules.Keys)
+                    {
+                        //if it matches the URL
+                        if (rule_part.IsMatch(request.uri))
+                        {
+                            //create a new response object
+                            //assign to it response from the method called as a delegate assigned to the URL rule
+                            webResposne toSend = serverRules[rule_part](request);
+
+                            //mark that we found a rule
+                            foundrule = true;
+
+                            //if the rule is meant to redirect...
+                            if (toSend.header.ContainsKey("Location"))
+                            {
+                                writ.WriteString("HTTP/1.1 302\r\n");
+                            }
+                            else //if a normal read operation
+                            {
+                                writ.WriteString("HTTP/1.1 200 OK\r\n");
+                            }
+                            
+                            //write content length to the buffer
+                            writ.WriteString("Content-Length: " + toSend.content.Length + "\r\n");
+
+                            //for each of the response headers (returned by the delegate assigned to the URL rule
+                            foreach (string key in toSend.header.Keys)
+                            {
+                                //write it to the output
+                                writ.WriteString(key + ": " + toSend.header[key] + "\r\n");
+                            }
+                            //add connection: close header
+                            writ.WriteString("Connection: close\r\n");
+
+                            //new line before writing content
+                            writ.WriteString("\r\n");
+  
+                            //we lock the webserver until response is finished
+                            lock (this)
+                            {
+                                //reset the output stream
+                                toSend.content.Seek(0, SeekOrigin.Begin);
+                                Task.Run(async () =>
+                                {
+                                    await writ.StoreAsync(); //wait for the data to be saved in the output
+                                    await writ.FlushAsync(); //flush (send to the output)
+
+                                    //write the data to the output using 1024 buffer (store and flush after every loop)
+                                    while (toSend.content.Position < toSend.content.Length)
+                                    {
+                                        byte[] buffer;
+                                        if (toSend.content.Length - toSend.content.Position < 1024)
+                                        {
+                                            buffer = new byte[toSend.content.Length - toSend.content.Position];
+                                        }
+                                        else
+                                        {
+                                            buffer = new byte[1024];
+                                        }
+                                        toSend.content.Read(buffer, 0, buffer.Length);
+                                        writ.WriteBytes(buffer);
+                                        
+                                        await writ.StoreAsync();
+                                        await writ.FlushAsync();
+                                    }
+                                    toSend.content.Close();
+                                });
+                            }
+                            break;
+                        }
+                    }
+                }
+                                
+                if (foundrule == false)
+                {
+                    writ.WriteString("HTTP/1.1 404 Not Found\r\n");
+                    writ.WriteString("Content-Type: text/html\r\n");
+                    writ.WriteString("Content-Length: 9\r\n");
+                    writ.WriteString("Pragma: no-cache\r\n");
+                    writ.WriteString("Connection: close\r\n");
+                    writ.WriteString("\r\n");
+                    writ.WriteString("Not found");
+                    Task.Run(async () =>
+                    {
+                        await writ.StoreAsync();
+                    });
+                }
+            }
+        }
+    }
+}
---
+++ SimpleWebServerExample/MainPage.xaml.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using SimpleWebServerExample.Resources;
+using System.Text.RegularExpressions;
+using IDCT;
+using System.IO;
+using System.IO.IsolatedStorage;
+
+
+namespace SimpleWebServerExample
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        IsolatedStorageFile isf = IsolatedStorageFile.GetUserStoreForApplication();
+        WebServer simpleWebServer;        
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+            createTestFileIfNotExist();
+        }
+
+        private void createTestFileIfNotExist()
+        {
+            if (isf.FileExists("test_file.txt")) return;
+            StreamWriter writer = new StreamWriter( isf.CreateFile("test_file.txt") );
+            writer.WriteLine("Device Name: " + Microsoft.Phone.Info.DeviceStatus.DeviceName);
+            writer.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            bool found = false;
+            string ip = "";
+
+            //get all network adapters
+            var adapters = Windows.Networking.Connectivity.NetworkInformation.GetHostNames();
+            if (adapters.Count == 0)
+            {
+                MessageBox.Show("Turn on wifi");
+            }
+            else
+            {
+                foreach (var adapter in adapters)
+                {
+                    //find the Wifi adapter (interface type == 71)
+                    if (adapter.IPInformation.NetworkAdapter.IanaInterfaceType == 71 && (adapter.Type == Windows.Networking.HostNameType.Ipv4 || adapter.Type == Windows.Networking.HostNameType.Ipv6))
+                    {
+                        //if found assign it's ip to a variable
+                        found = true;
+                        ip = adapter.RawName;
+                        break;
+                    }
+                }
+
+                if (found == true)
+                {
+                    //create a new dictionary for server rules
+                    Dictionary<Regex, RuleDeletage> rules = new Dictionary<Regex, RuleDeletage>();
+
+                    //add a rule for homepage: url /
+                    //will fire method "homePage" when triggered
+                    Regex rgx = new Regex("^/$");
+                    rules.Add(rgx, homePage);
+
+                    //add a rule for file any url under subfolder /files 
+                    //this is a file download example
+                    Regex rgx_file = new Regex("^/files/.*$");
+                    rules.Add(rgx_file, getfile);
+
+                    //with the set of rules and IP of the network adapter create a new web server object and assign an error event method
+                    simpleWebServer = new WebServer(rules, ip, "80");
+                    simpleWebServer.errorOccured += ws_errorOccured;
+
+                    ((Button)sender).IsEnabled = false;
+                    ipinfo.Text = "With another device in the network\r\ngo to:\r\nhttp://" + ip + "/";
+                }
+                else
+                {
+                    MessageBox.Show("Turn on wifi");
+                }
+            }
+        }
+
+        webResposne homePage(webResposne request)
+        {
+            //prepare the response object
+            webResposne newResponse = new webResposne();
+
+            //create a new dictionary for headers - this could be done using a more advanced class for webResponse object - i just used a simple struct
+            newResponse.header = new Dictionary<string, string>();
+
+            //add content type header
+            newResponse.header.Add("Content-Type", "text/html");
+
+            Stream resposneText = new MemoryStream();
+            StreamWriter contentWriter = new StreamWriter(resposneText);
+            contentWriter.WriteLine("<html>");
+            contentWriter.WriteLine("   <head>");
+            contentWriter.WriteLine("       <title>Sample response</title>");
+            contentWriter.WriteLine("   </head>");
+            contentWriter.WriteLine("   <body>");
+            contentWriter.WriteLine("   <p>Phone info:</p>");
+            contentWriter.WriteLine("   <p><b>Platform: </b>" + Environment.OSVersion.Platform + "</p>");
+            contentWriter.WriteLine("   <p><b>Device Name: </b>" + Microsoft.Phone.Info.DeviceStatus.DeviceName + "</p>");
+            contentWriter.WriteLine("   <p>Download file</p>");
+            contentWriter.WriteLine("   <p><a href='/files/test_file.txt'>get file</a></p>");
+            contentWriter.WriteLine("   </body>");
+            contentWriter.WriteLine("</html>");
+            contentWriter.Flush();
+            //assign the response
+            newResponse.content = resposneText;
+
+            //return the response
+            return newResponse;
+        }
+
+        webResposne getfile(webResposne request)
+        {
+            webResposne newResponse = new webResposne();
+
+            newResponse.header = new Dictionary<string, string>();
+            Stream resposneText = new MemoryStream();
+
+            //get the file name from the uri by removing the prefix
+            string filename = request.uri.Replace("/files/", "");
+
+            
+            if (isf.FileExists("/" + filename))
+            {
+                //set thea content type header
+                newResponse.header.Add("Content-Type", "binary/octet-stream");
+
+                //inform that it is going to be a download with a filename as in the request
+                newResponse.header.Add("Content-Disposition", "attachment; filename=" + filename);
+
+                //assign the file to the request
+                resposneText = isf.OpenFile("/" + filename, FileMode.Open);
+            }
+            else
+            {
+                //file not found
+                newResponse.header.Add("Content-Type", "text/html");
+                                
+                StreamWriter contentWriter = new StreamWriter(resposneText);
+                contentWriter.WriteLine("<html>");
+                contentWriter.WriteLine("   <head>");
+                contentWriter.WriteLine("       <title>Not found</title>");
+                contentWriter.WriteLine("   </head>");
+                contentWriter.WriteLine("   <body>");
+                contentWriter.WriteLine("   <p>File not found</p>");
+                contentWriter.WriteLine("   </body>");
+                contentWriter.WriteLine("</html>");
+                contentWriter.Flush();
+                //assign the response
+                newResponse.content = resposneText;
+            }
+
+            newResponse.content = resposneText;
+
+            return newResponse;
+        }
+
+        void ws_errorOccured(int code, string message)
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+            {
+                MessageBox.Show("Error: " + message);
+            });
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
